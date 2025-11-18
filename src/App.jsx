c9f2b3c40fcd599e136c83ff8fd061047b283a9f
sukacode <firmansahalhadi@gmail.com>
2025-11-18 18:02:10 +0700

    feat(AboutSection): add about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import DataImage from "./data";
 function App() {
   return (
     <>
-      <div className="hero grid grid-cols-2">
+      <div className="hero grid md:grid-cols-2 items-center pt-6 xl:gap-0 gap-6">
         <div>
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit   p-4 rounded-2xl">
             <img
@@ -37,8 +37,49 @@ function App() {
         <img
           src={DataImage.HeroImage}
           alt="Image"
-          className="w-[400px] ml-auto"
+          className="w-[400px] md:ml-auto rounded-b-4xl opacity-60"
         />
+      </div>
+
+      {/* About */}
+      <div className="about mt-32 py-10">
+        <div className="w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+          <img
+            src={DataImage.HeroImage}
+            alt="Image"
+            className="w-12 rounded-md mb-10 sm:hidden"
+          />
+          <p className="text-base/loose mb-10">
+            Didirikan di Jakarta pada tahun 2025, kami hadir untuk mendukung
+            kebutuhan industri maritim Indonesia yang semakin berkembang,
+            terutama dalam layanan ship repair, maintenance, dan engineering
+            support untuk kapal niaga, kapal tunda, maupun kapal pemerintah.
+            Dengan tenaga ahli berpengalaman dan sistem kerja profesional, kami
+            bertekad menjadi mitra terpercaya bagi perusahaan galangan, operator
+            kapal, dan instansi maritim di seluruh Indonesia.
+          </p>
+          <div className="flex items-center justify-between">
+            <img
+              src={DataImage.HeroImage}
+              alt="Image"
+              className="w-12 rounded-md sm:block hidden"
+            />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">
+                  45 <span className="text-violet-500">+</span>
+                </h1>
+                <p>Proyek Selesai</p>
+              </div>
+              <div>
+                <h1 className="text-4xl mb-1">
+                  4 <span className="text-violet-500">+</span>
+                </h1>
+                <p>Tahun Pengalaman</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
