7022485f441f4518f704dcaab995ccb2f5461b63
sukacode <firmansahalhadi@gmail.com>
2025-12-06 22:37:28 +0700

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ const Footer = () => {
     <div className="mt-32 py-4 w-full flex md:flex-row flex-col gap-6 md:gap-0 justify-center items-center">
       {/* <h1 className="text-2xl font-bold">PT. Elang Marine</h1> */}
       <p className="flex items-center text-center text-sm text-gray-400">
-        LEGALYN – CIPUTRA INTERNASIONAL,
+        CIPUTRA INTERNASIONAL,
         <br />
         TOKOPEDIA CARE TOWER, LT 15 UNIT 10 JAKARTA BARAT – INDONESIA
         <br />
         <br />
         email : support@elangmarine.com
         <br />
-        elangmarineteknindo©️2025
+        ©️Copyright 2025. All Rights Reserved by ZIT
       </p>
 
       {/* <div className="flex gap-7">
